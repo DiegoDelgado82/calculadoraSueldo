2e65602f755405a7871b34df906fed5f17c96299
Diego Delgado <diegodelgado365@gmail.com>
2023-10-23 22:02:18 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const calcularSueldo = ()=>{
+let basico= document.getElementById("sueldoBasico").value 
+let domingo= document.getElementById("horasDomingo").value *0.00499* basico
+let feriado= document.getElementById("horasFeriado").value *0.01*basico
+let sueldo= (parseInt(domingo)+parseInt(feriado)+parseInt(basico))*0.83
+document.getElementById("sueldo").innerText=`$${sueldo}`
+}
