cd2d77360fc83ae3efd22349e4796f5499db819a
Diego Delgado <diegodelgado365@gmail.com>
2023-10-24 10:11:46 -0300

    aplicando estilos a la página

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const calcularSueldo = ()=>{
 let basico= document.getElementById("sueldoBasico").value 
 let domingo= document.getElementById("horasDomingo").value *0.00499* basico
 let feriado= document.getElementById("horasFeriado").value *0.01*basico
-let sueldo= (parseInt(domingo)+parseInt(feriado)+parseInt(basico))*0.83
+let nocturnas= document.getElementById("horasNocturnas").value *0.000649*basico
+let comisiones= document.getElementById("comisiones").value*1
+let sueldo= (parseInt(domingo)+parseInt(feriado)+parseInt(basico)+parseInt(nocturnas)+parseInt(comisiones))*0.83
 document.getElementById("sueldo").innerText=`$${parseInt(sueldo)}`
+
 }
+
+parseFloat()
