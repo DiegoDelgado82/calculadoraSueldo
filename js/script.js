6088ece072560b15ed4c0fbaf3e902fa19c95f5f
Diego Delgado <diegodelgado365@gmail.com>
2024-01-20 11:40:32 -0300

    modificación de campos y agregado de sueldo bruto

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let basico= document.getElementById("sueldoBasico").value
 let domingo= document.getElementById("horasDomingo").value *0.00499* basico
 let feriado= document.getElementById("horasFeriado").value *0.01*basico
 let nocturnas= document.getElementById("horasNocturnas").value *0.000649*basico
-let comisiones= document.getElementById("comisiones").value*1
-let sueldo= (parseInt(domingo)+parseInt(feriado)+parseInt(basico)+parseInt(nocturnas)+parseInt(comisiones))*0.83
+let sueldo= (parseInt(domingo)+parseInt(feriado)+parseInt(basico)+parseInt(nocturnas))*0.83
+let bruto= parseInt(domingo)+parseInt(feriado)+parseInt(basico)+parseInt(nocturnas)
 document.getElementById("sueldo").innerText=`$${parseInt(sueldo)}`
-
+document.getElementById("bruto").innerText=`$${parseInt(bruto)}`
 }
 
 parseFloat()
